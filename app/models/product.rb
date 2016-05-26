96ec2859899f0bb1d42dbe561e5a57bc5be96d39
jclaudio.7@gmail.com <jclaudiovenancio@gmail.com>
2016-05-26 13:06:42 -0300

    Adds search class method to filter products

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,17 @@ class Product < ActiveRecord::Base
 
   belongs_to :user
 
+  def self.search(params = {})
+    products = params[:product_ids].present? ? Product.find(params[:product_ids]) : Product.all
+
+    products = products.filter_by_title(params[:keyword]) if params[:keyword]
+    products = products.above_or_equal_to_price(params[:min_price]) if params[:min_price]
+    products = products.below_or_equal_to_price(params[:max_price]) if params[:max_price]
+    products = products.recent(params[:recent]) if params[:recent].present?
+
+    products
+  end
+
   scope :filter_by_title, lambda {|keyword|
     where("lower(title) LIKE ?", "%#{keyword.downcase}%")
   }
